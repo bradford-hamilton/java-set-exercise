7f2a7970e89676c42c9cc1ecb837816028cb5bf3
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-08-30 15:33:44 -0600

    initial commit, add java set exercise

--- src/main/java/Set.java
+++ src/main/java/Set.java
@@ -2,24 +2,58 @@ package main;
 
 public class Set {
 
+  private boolean isEmpty = true;
+  private int size = 0;
+  private int[] myIntArray = new int[100];
+  private int currentIndex = 0;
+
   public boolean IsEmpty() {
-    return true;
+    return isEmpty;
   }
 
   public boolean Add(int value) {
-    return false;
+    boolean contains = Contains(value);
+    if (!contains) {
+      myIntArray[currentIndex] = value;
+      currentIndex++;
+      isEmpty = false;
+      size++;
+      return false;
+    }
+    return true;
   }
 
   public int Size() {
-    return 0;
+    return size;
   }
 
   public boolean Contains(int value) {
+    for (int i = 0; i < currentIndex; i++) {
+      if ( myIntArray[i] == value ) {
+        return true;
+      }
+    }
     return false;
   }
 
   public boolean Remove(int value) {
-    return false;
+    int foundIndex = -1;
+    for (int i = 0; i < currentIndex; i++) {
+      if ( myIntArray[i] == value ) {
+        foundIndex = i;
+        break;
+      }
+    }
+    if ( foundIndex == -1 ) {
+      return false;
+    } else {
+      for (int i = foundIndex; i < currentIndex; i++) {
+        myIntArray[i] = myIntArray[i + 1];
+      }
+      size--;
+      currentIndex--;
+      return true;
+    }
   }
 
   public void Clear() {
@@ -27,7 +61,11 @@ public class Set {
   }
 
   public int[] Enumerate() {
-    return new int[0];
+    int[] values = new int[currentIndex];
+    for (int i = 0; i < currentIndex; i++) {
+      values[i] = this.myIntArray[i];
+    }
+    return values;
   }
 
 }
